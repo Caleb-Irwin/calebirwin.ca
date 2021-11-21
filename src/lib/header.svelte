52c53fde3e905112233f477563281308669ecd91
Caleb-Irwin <56744087+Caleb-Irwin@users.noreply.github.com>
2021-11-20 21:21:58 -0600

    Nonfunctional Navbar

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,9 +1,24 @@
 <script>
   import darkMode from '$lib/darkModeStore';
+  import Buttom from '$lib/headerUtils/darkModeButton.svelte';
+  import Logo from '$lib/headerUtils/logo.svelte';
+  import Burger from '$lib/headerUtils/burgerIcon.svelte';
 </script>
 
-<div class="flex w-full">
-  <h1>Header</h1>
+<div class="flex h-16 m-4">
+  <div class="w-16 h-16 flex flex-row justify-center items-center">
+    <Burger primaryColour={$darkMode ? '#FAFAFA' : '#18181B'} />
+  </div>
   <div class="flex-grow" />
-  <button on:click={darkMode.toggle}>Toggle Dark Mode</button>
+  <Logo
+    primaryColour={$darkMode ? '#FAFAFA' : '#18181B'}
+    secondaryColour={$darkMode ? '#312E81' : '#A5B4FC'}
+  />
+  <div class="flex-grow" />
+  <div class="h-16">
+    <Buttom
+      primaryColour={$darkMode ? '#FAFAFA' : '#18181B'}
+      secondaryColour={$darkMode ? '#312E81' : '#A5B4FC'}
+    />
+  </div>
 </div>
