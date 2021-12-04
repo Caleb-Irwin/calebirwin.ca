c2e7516bb8922c41a05534403b66652a2b33f4f2
Caleb-Irwin <56744087+Caleb-Irwin@users.noreply.github.com>
2021-12-04 13:21:34 -0600

    very simple bio

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,3 +1,20 @@
-<div>
-  <h1 class="text-2xl">About</h1>
+<script>
+  let today = new Date(),
+    age = today.getFullYear() - 2006 - 1,
+    month = today.getMonth(),
+    day = today.getDate();
+
+  if (month > 3) {
+    age++;
+  } else if (month === 3 && day >= 15) {
+    age++;
+  }
+</script>
+
+<div class="flex-grow flex flex-col justify-center items-center">
+  <h1 class="text-5xl">About</h1>
+  <p class="p-4">
+    Caleb Irwin is a {age} year old developer who has been writing code since he was 10. (Extended bio
+    comming soon)
+  </p>
 </div>
