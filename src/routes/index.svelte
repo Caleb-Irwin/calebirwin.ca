8e75aa00e03f799990e0ba909b93a3a0a3af81dd
Caleb-Irwin <56744087+Caleb-Irwin@users.noreply.github.com>
2021-12-04 11:50:23 -0600

    Simple index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,56 @@
-<h1 class="text-2xl">Caleb Irwin</h1>
-<p>Body</p>
+<script>
+  import darkMode from '$lib/darkModeStore';
+</script>
+
+<div class="flex-grow flex flex-col justify-center items-center">
+  <h1 class="text-8xl text-center font-black {$darkMode ? 'dark' : 'light'}">Caleb Irwin</h1>
+</div>
+
+<style lang="postcss">
+  h1 {
+    color: #00000000;
+    /* background: linear-gradient(to right, #18181b 0%, white 100%); */
+    -webkit-background-clip: text;
+    /* -webkit-text-fill-color: transparent; */
+    /* animation: nameDark 5s linear 2s infinite alternate; */
+  }
+
+  .dark {
+    -webkit-text-stroke: 1.5px white;
+    text-stroke: 1.5px white;
+    animation: nameDark 5s linear 2s infinite alternate;
+  }
+  .light {
+    -webkit-text-stroke: 1.5px black;
+    text-stroke: 1.5px black;
+    animation: nameLight 5s linear 2s infinite alternate;
+  }
+  @keyframes nameDark {
+    0% {
+      background-color: #312e81;
+    }
+    50% {
+      background-color: #18181b;
+    }
+    75% {
+      background-color: #312e81;
+    }
+    100% {
+      background-color: #312e81;
+    }
+  }
+  @keyframes nameLight {
+    0% {
+      background-color: #a5b4fc;
+    }
+    50% {
+      background-color: #fafafa;
+    }
+    75% {
+      background-color: #a5b4fc;
+    }
+    100% {
+      background-color: #a5b4fc;
+    }
+  }
+</style>
