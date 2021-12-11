31dec94897f534c1035f6da0ee001fa71f4652e9
Caleb-Irwin <56744087+Caleb-Irwin@users.noreply.github.com>
2021-12-11 12:39:00 -0600

    removed unused

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,52 +4,5 @@
 </script>
 
 <div class="flex-grow flex flex-col justify-center items-center">
-  <!-- <h1 class="text-8xl text-center font-black {$darkMode ? 'dark' : 'light'}">Caleb Irwin</h1> -->
   <Name />
 </div>
-
-<style lang="postcss">
-  h1 {
-    color: #00000000;
-    -webkit-background-clip: text;
-  }
-
-  .dark {
-    -webkit-text-stroke: 1.5px white;
-    text-stroke: 1.5px white;
-    animation: nameDark 10s linear 0s infinite normal;
-  }
-  .light {
-    -webkit-text-stroke: 1.5px black;
-    text-stroke: 1.5px black;
-    animation: nameLight 10s linear 0s infinite normal;
-  }
-  @keyframes nameDark {
-    0% {
-      background-color: #312e81;
-    }
-    25% {
-      background-color: #18181b;
-    }
-    50% {
-      background-color: #312e81;
-    }
-    100% {
-      background-color: #312e81;
-    }
-  }
-  @keyframes nameLight {
-    0% {
-      background-color: #a5b4fc;
-    }
-    50% {
-      background-color: #fafafa;
-    }
-    /* 50% {
-      background-color: #a5b4fc;
-    } */
-    100% {
-      background-color: #a5b4fc;
-    }
-  }
-</style>
