856726c621bb175dd59103a2e3771c87a15ead8e
Caleb-Irwin <56744087+Caleb-Irwin@users.noreply.github.com>
2021-12-18 18:42:20 -0600

    more horrible debugging

--- src/routes/api/contact.ts
+++ src/routes/api/contact.ts
@@ -44,7 +44,13 @@ export const post = async (req: Request): Promise<Response> => {
         location: `/contact?failed&name=${encodeURIComponent(
           body.get('name')
         )}&email=${encodeURIComponent(body.get('email'))}&message=${encodeURIComponent(
-          err + '\n' + import.meta.env.VITE_LF_SOURCE_TOKEN + '\n' + body.get('message')
+          err +
+            '\n' +
+            import.meta.env.VITE_LF_SOURCE_TOKEN +
+            '\n' +
+            import.meta.env.VITE_STATIC_FORMS_API_KEY +
+            '\n' +
+            body.get('message')
         )}`
       }
     };
