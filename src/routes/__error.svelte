eaac284a9c4a86481180abc00108d6c6d00cd0f4
Caleb-Irwin <56744087+Caleb-Irwin@users.noreply.github.com>
2022-01-02 12:01:26 -0600

    fixed error styling

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,21 +1,25 @@
 <script lang="ts" context="module">
-	import type { ErrorLoad } from '@sveltejs/kit';
-	export const load: ErrorLoad = ({ error, status }) => {
-		console.error(error);
-		return {
-			props: {
-				title: `${status}`,
-				message: `${error.message}`
-			}
-		};
-	};
+  import type { ErrorLoad } from '@sveltejs/kit';
+  export const load: ErrorLoad = ({ error, status }) => {
+    console.error(error);
+    return {
+      props: {
+        title: `${status}`,
+        message: `${error.message}`
+      }
+    };
+  };
 </script>
 
 <script>
-	export let title;
-	export let message;
+  export let title;
+  export let message;
 </script>
 
-<h1 class="text-6xl text-center">{title}</h1>
-<p class="text-2xl text-center">{message}</p>
-<a class="text-2xl text-center underline" href="/">Home</a>
+<div class="flex-grow">
+  <h1 class="text-6xl text-center">{title}</h1>
+  <p class="text-2xl text-center">{message}</p>
+  <p class="text-2xl text-center underline">
+    <a href="/">Home</a>
+  </p>
+</div>
