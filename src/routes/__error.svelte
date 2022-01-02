1e0c8eac0d2f4db3cd8204839ff90c2d7f4a267a
Caleb-Irwin <56744087+Caleb-Irwin@users.noreply.github.com>
2022-01-01 18:07:28 -0600

    added custom error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,21 @@
+<script lang="ts" context="module">
+	import type { ErrorLoad } from '@sveltejs/kit';
+	export const load: ErrorLoad = ({ error, status }) => {
+		console.error(error);
+		return {
+			props: {
+				title: `${status}`,
+				message: `${error.message}`
+			}
+		};
+	};
+</script>
+
+<script>
+	export let title;
+	export let message;
+</script>
+
+<h1 class="text-6xl text-center">{title}</h1>
+<p class="text-2xl text-center">{message}</p>
+<a class="text-2xl text-center underline" href="/">Home</a>
